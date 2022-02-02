a06c02a3358f6e428434fd44c78584fad458ce5c
mayurjadhav09 <mayurjadhav270@gmail.com>
2022-02-02 20:45:16 +0530

    day5

---
+++ day_5/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect } from 'react';
+
+function App() {
+
+  console.log("1")
+  useEffect(()=>{
+    //Mounting
+    console.log("2")
+  })
+
+  console.log("3")
+  return (
+    <div className="App">
+
+  
+    
+    </div>
+  );
+}
+
+export default App;
